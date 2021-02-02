96ae7ae7f97f59aff05cdc66043a6f435fa7d59b
Michael Fuget <michaelfuget@gmail.com>
2021-02-02 05:06:01 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
 });
+document.addEventListener('submit', (event) =>{
+  event.preventDefault()
+  const list = document.getElementById('tasks')
+  const newTaskData = document.getElementById('new-task-description').value
+  const taskListNode = document.createElement("LI");
+  const textNode = document.createTextNode(newTaskData);
+  taskListNode.appendChild(textNode)
+  list.appendChild(taskListNode)
+})

5cb1f3a67f3f43555269b54df88d209b27669800
Michael Fuget <michaelfuget@gmail.com>
2021-02-02 05:02:03 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 document.addEventListener('submit', (event) =>{
   event.preventDefault()
+  const list = document.getElementById('tasks')
+  const new_task_data = document.getElementById('new-task-description').value
 })

ad488f7116042170913500046d034bf976161b19
Michael Fuget <michaelfuget@gmail.com>
2021-02-02 05:04:30 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -4,5 +4,9 @@ document.addEventListener("DOMContentLoaded", () => {
 document.addEventListener('submit', (event) =>{
   event.preventDefault()
   const list = document.getElementById('tasks')
-  const new_task_data = document.getElementById('new-task-description').value
+  const newTaskData = document.getElementById('new-task-description').value
+  const taskListNode = document.createElement("LI");
+  const textNode = document.createTextNode(newTaskData);
+  taskListNode.appendChild(textNode)
+  list.appendChild(taskListNode)
 })
